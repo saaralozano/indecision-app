4cb1ffaf6033fd1ef5a3169112bcec9e7dfa8895
Sara Lozano <saloz2001@gmail.com>
2025-02-20 19:42:34 +0100

    Primer componente

---
+++ src/components/MeusNumerator.vue
@@ -0,0 +1,29 @@
+
+<template>
+    <section>
+
+        <h3>Counter: {{ numerus }}</h3>
+        <h3>Square: {{ quadrataNumerus }}</h3>
+    
+
+        <div>
+            <button @click="numerus++">+1</button>
+            <button @click="numerus--">-1</button>
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+    import { computed, ref } from 'vue';
+
+    interface Props {
+        valorem: number;
+    }
+
+    const props = defineProps<Props>();
+
+    const numerus = ref(props.valorem);
+    const quadrataNumerus = computed(() => numerus.value * numerus.value );
+
+
+</script>
