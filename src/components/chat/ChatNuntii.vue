b6723d8bf69233d2671d19d64af03182471667a3
Sara Lozano <saloz2001@gmail.com>
2025-02-25 20:39:57 +0100

    api yesno

--- src/components/chat/ChatNuntii.vue
+++ src/components/chat/ChatNuntii.vue
@@ -1,20 +1,18 @@
 
 
 <template>
+
     <div class="p-4 flex-1 overflow-y-auto">
-
         <div class="flex flex-col space-y-2">
-            <div class="flex justify-end">
-                <div class="bg-blue-200 text-black p-2 max-w-xs rounded-lg">
-                    Hola, ¿Cómo te va el día?
-                </div>
-            </div>
 
-            <div class="flex">
-                <div class="bg-gray-300 text-black p-2 max-w-xs rounded-lg">
-                    Muy bien, estoy en casa. ¿Y tú?
-                </div>
-            </div>
+            <ChatBullula :meus-est="true" nuntius="¿Salimos luego a dar una vuelta?" />
+
         </div>
     </div>
+
 </template>
+
+<script lang="ts" setup>
+import ChatBullula from './ChatBullula.vue'
+
+</script>
