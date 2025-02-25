798cbe55d559701998f46c42647537ef01a4e78f
Sara Lozano <saloz2001@gmail.com>
2025-02-25 20:06:21 +0100

    Pantalla de chat

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 
 <template>
-   <h1>Mi primera app de Vue</h1>
 
-   <h1 class="text-5xl text-rose-800 font-bold underline">
-    Hello world!
-   </h1>
-
-   <MeusNumerator :valorem="7" />
-   <MeusNumeratorScript :valorem="5" />
-   <MeusNumeratorScript2 :valorem="2" />
+<IndecisionView />
 
 </template>
 
-<script lang="ts" setup>
-  import MeusNumerator from './components/MeusNumerator.vue';
-  import MeusNumeratorScript from './components/MeusNumeratorScript.vue'
-  import MeusNumeratorScript2 from './components/meus-numerator-script/MeusNumeratorScript2.vue'
 
+<script lang="ts" setup>
+
+import IndecisionView from './views/IndecisionView.vue';
 
 </script>
